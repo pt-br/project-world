d2c25682a7a5ae4846927faff103401c92c1d6e6
Lucas Paixão <lucas.paixao@objectedge.com>
2015-05-01 02:46:11 -0300

    New features: Talking, Make friends and Make enemies

--- js/main.js
+++ js/main.js
@@ -24,6 +24,8 @@ $(function() {
     $this.thinkTime;
     $this.busy = false;
     $this.enjoyingConversation = 0;
+    $this.friends = [];
+    $this.enemies = [];
     $this.nextTo = [];
 
     jQuery("body").append("<div class='bot' id='"+ $this.name +"'></div>");
@@ -53,6 +55,7 @@ $(function() {
     $$world = new World("Project01");
     $$world.setDayTime();
     loadWords("lib/words.txt");
+    //loadWords("lib/test.txt");
 
     $botList = [];
     $$botHonki = new Bot("Honki");
@@ -106,9 +109,8 @@ $(function() {
     console.log("[ " + this.getName() + " ] I'm bored. I don't wanna do nothing now...");
   }
   Bot.prototype.walk = function() {
-    console.log("[ " + this.getName() + " ] I'd like to take a walk!");
-      
     var $this = this;
+    console.log("[ " + $this.getName() + " ] I'd like to take a walk!");
 
     cleanProximity($this);
 
@@ -127,9 +129,6 @@ $(function() {
       checkProximity();
     });  
   }
-  Bot.prototype.say = function() {
-    var $phraseSize = Math.floor((Math.random() * 5000) + $manualThinkTime);
-  }
   Bot.prototype.talk = function() {
     var $this = this;
     console.log("[ " + $this.getName() + " ] I wanna talk a little, but with who?");
@@ -145,9 +144,78 @@ $(function() {
         // Partner is not busy, it will start a chat
         $talkingPartner.busy = true;
         console.log("[ " + $this.getName() + " ] Aha! I'll talk to " + $talkingPartner.getName());
+        
+        var $nextPhraseTime = Math.floor((Math.random() * 5000) + 2000);
+        var $talkingNow = 1;
+        var $chatInterval = setInterval(function() {
+          if($this.enjoyingConversation > -3 && $this.enjoyingConversation < 3 && $talkingPartner.enjoyingConversation > -3 && $talkingPartner.enjoyingConversation < 3) {
+            // Keep talking
+            var $toSay = say();
+            
+            if($talkingNow == 1) {
+              console.log("[ CONVERSATION ] " + $this.getName() + " says to " + $talkingPartner.getName() + ": " + $toSay);
+              
+              var $like = Math.floor(Math.random() * 2);
+              if($like == 0) {
+                // Didn't like 
+                console.log("[ THINKING ] " + $talkingPartner.getName() + " thinks: Hmm... I didn't like this...");
+                $talkingPartner.enjoyingConversation--;
+              } else {
+                // Like
+                console.log("[ THINKING ] " + $talkingPartner.getName() + " thinks: Wow! Interesting");
+                $talkingPartner.enjoyingConversation++;
+              }
 
+              $talkingNow = 2;
 
+            } else {
+              console.log("[ CONVERSATION ] " + $talkingPartner.getName() + " says to " + $this.getName() + ": " + $toSay);
+              
+              var $like = Math.floor(Math.random() * 2);
+              if($like == 0) {
+                // Didn't like 
+                console.log("[ THINKING ] " + $this.getName() + " thinks: Hmm... I didn't like this...");
+                $this.enjoyingConversation--;
+              } else {
+                // Like
+                console.log("[ THINKING ] " + $this.getName() + " thinks: Wow! Interesting");
+                $this.enjoyingConversation++;
+              }
 
+              $talkingNow = 1;
+            }
+            $nextPhraseTime = Math.floor((Math.random() * 10000) + 5000);
+          } else {
+            // Stop the talking
+            clearInterval($chatInterval);
+            $this.busy = false;
+            $talkingPartner.busy = false;
+
+            // Define new friends/enemies
+            if($this.enjoyingConversation == 3) {
+              $this.friends.push($talkingPartner);
+              console.log("[ CONVERSATION ] " + $this.getName() + " says to " + $talkingPartner.getName() + ": I really liked to talk to you, consider yourself my friend! Goodbye");
+            }
+            else if($this.enjoyingConversation == -3) {
+              $this.enemies.push($talkingPartner);
+              console.log("[ CONVERSATION ] " + $this.getName() + " says to " + $talkingPartner.getName() + ": You are a bullshit! Don't be crazy to get in my way again!");
+            }
+            else if($talkingPartner.enjoyingConversation == 3) {
+              $talkingPartner.friends.push($this);
+              console.log("[ CONVERSATION ] " + $talkingPartner.getName() + " says to " + $this.getName() + ": I really liked to talk to you, consider yourself my friend! Goodbye");
+            }
+            else if($talkingPartner.enjoyingConversation == -3) {
+              $talkingPartner.enemies.push($this);
+              console.log("[ CONVERSATION ] " + $talkingPartner.getName() + " says to " + $this.getName() + ": You are a bullshit! Don't be crazy to get in my way again!");
+            }
+
+            // Clean the enjoyingConversation value
+            $this.enjoyingConversation = 0;
+            $talkingPartner.enjoyingConversation = 0;
+
+          }
+        }, $nextPhraseTime);
+        
       } else {
         // Partner busy, can't talk now
         console.log("[ " + $this.getName() + " ] Oh... I would talk with " + $talkingPartner.getName() + " but he's busy now :/");
@@ -201,11 +269,23 @@ $(function() {
       if($rawFile.readyState === 4) {
         if($rawFile.status === 200 || $rawFile.status == 0) {
           var $allText = $rawFile.responseText;
-          $$wordList = $allText.split(",");
+          $$wordList = $allText.split(/\n/);
         }
       }
     } 
     $rawFile.send(null);
+  }
+
+  function say() {
+    var $wordsMax = $$wordList.length - 1;
+    var $phraseSize = Math.floor((Math.random() * 5) + 1);
+    var $phrase = "";
+    for(var i = 0; i <= $phraseSize; i++) {
+      var $wordIndex = Math.floor(Math.random() * $wordsMax);
+      var $word = $$wordList[$wordIndex];
+      $phrase = $phrase + " " + $word; 
+    }
+    return $phrase;
   }
 
   function cleanProximity($$botObject) {
