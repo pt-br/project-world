23abc3aff0f8fa7dbeaee1e92a4180e5b8103379
pt-br <lucas.paixao@objectedge.com>
2015-10-18 05:42:39 -0200

    Fixing helvetica request error

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var io = require('socket.io')(http);
 
 app.use("/css", express.static(__dirname + '/css'));
 app.use("/lib", express.static(__dirname + '/lib'));
+app.use("/fonts", express.static(__dirname + '/fonts'));
 app.use("/js", express.static(__dirname + '/js'));
 app.use("/images", express.static(__dirname + '/images'));
 app.use('/favicon.ico', express.static('favicon.ico'));
